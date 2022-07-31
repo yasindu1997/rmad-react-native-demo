67fbfd6800ecc972a2557d76539b9d160d2bbbb3
yasindu1997 <yasindusathsara@gmail.com>
2022-07-31 11:29:33 +0530

    firestore add, update, delete done

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import { View } from 'react-native'
 import React from 'react'
 import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
+import AddData from './pages/AddData'
 
 export default function App() {
   return (
-   <SignIn/>
+    <AddData />
   )
 }
---
+++ pages/AddData.js
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from 'react'
+import { Center, Container, NativeBaseProvider, Text, Input, Button, Icon } from 'native-base'
+import firestore from '@react-native-firebase/firestore';
+
+export default function AddData() {
+    const [name, setName] = useState('');
+    const [address, setAddress] = useState('');
+    const [age, setAge] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+
+    useEffect(() => {
+        const subscriber = firestore()
+            .collection('students')
+            .doc('JdUy4QRW0RLKk3NuKpAt')
+            .onSnapshot(documentSnapshot => {
+                setAddress(documentSnapshot.data().address);
+                console.log('User data: ', documentSnapshot.data());
+            });
+
+        // Stop listening for updates when no longer required
+        return () => subscriber();
+    });
+
+    const saveStudent = async () => {
+        await firestore()
+            .collection('students')
+            .add({
+                name: name,
+                age: age,
+                address: address,
+                email: email,
+                phone: phone
+            })
+            .then(() => {
+                console.log('Student added!');
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+    }
+
+    const deleteStudent = async () => {
+        await firestore()
+            .collection('students')
+            .doc('0nwj80BIrUL8GYqZlTeZ')
+            .delete()
+            .then(() => {
+                console.log('User deleted!');
+            });
+    }
+
+
+    const updateStudent = async () => {
+        await firestore()
+            .collection('students')
+            .doc('JdUy4QRW0RLKk3NuKpAt')
+            .update({
+                age: 31,
+            })
+            .then(() => {
+                console.log('User updated!');
+            });
+    }
+
+    return (
+        <NativeBaseProvider>
+            <Center>
+                <Container>
+                    <Text fontSize="3xl" bold underline mt="35%" color="rose.800" ml="24%">Add Student</Text>
+                    <Input variant="rounded" value={name} onChangeText={txt => setName(txt)} placeholder="Name" mt="20" mb="5%" />
+                    <Input variant="rounded" value={age} onChangeText={txt => setAge(txt)} placeholder="Age" mb="5%" />
+                    <Input variant="rounded" value={address} onChangeText={txt => setAddress(txt)} placeholder="Address" mb="5%" />
+                    <Input variant="rounded" value={email} onChangeText={txt => setEmail(txt)} placeholder="Email" mb="5%" />
+                    <Input variant="rounded" value={phone} onChangeText={txt => setPhone(txt)} placeholder="Phone" mb="5%" />
+                    <Button size="sm" colorScheme="green" pl="10" pr="10" mt="10" ml="30%" onPress={saveStudent}>
+                        Add Student
+                    </Button>
+                    <Button size="sm" colorScheme="red" pl="10" pr="10" mt="5" ml="30%" onPress={deleteStudent}>
+                        Remove Student
+                    </Button>
+                    <Button size="sm" colorScheme="yellow" pl="10" pr="10" mt="5" ml="30%" onPress={updateStudent}>
+                        Update Student
+                    </Button>
+                </Container>
+            </Center>
+        </NativeBaseProvider>
+    )
+}
